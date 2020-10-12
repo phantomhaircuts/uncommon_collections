2066ac45eadd5ad6ed6c7105dfd725e655250d05
Erik Anderson <erik@Eriks-MacBook-Pro.local>
2020-10-12 13:25:48 -0400

    git ignore

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 let Airtable = require('airtable');
-let base = new Airtable({apiKey: 'key892LAYQL8OqDjw'}).base('appAKhLv2XNhfzfF4');
+let base = new Airtable({apiKey: config.apiKey}).base('appAKhLv2XNhfzfF4');
 
 base('Table 1').select({
     maxRecords: 100,
@@ -9,7 +9,9 @@ base('Table 1').select({
     records.forEach(function(record) {
         let masonry = document.querySelector('.masonry');
         masonry.innerHTML += `
+        <div class='img-wrapper'>
             <img src='${record.fields.Attachments[0].thumbnails.large.url}'/>
+            <div class='hidden overlay'>${record.get('Caption') || 'This is filler'}</div>
         </div>
     `
         console.log('Retrieved', record.get('Caption'));
